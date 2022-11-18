3cfdb2c2bd4263e3c1e081879ed24298cd941c4c
carlosjtandem <soycarlosjavier14@gmail.com>
2022-11-18 12:11:53 -0500

    Iota

--- DataTypes/types.go
+++ DataTypes/types.go
@@ -35,4 +35,20 @@ func main() {
 	const c int = 3 // Implicit int
 	//fmt.Println(c + 1.15)  // Erro because not is posible to add a int and float
 	fmt.Println(float32(c) + 1.15) // Erro because not is posible to add a int and float
+
+	// IODA  // variable that increments in 1 each call
+	fmt.Println("IOTA")
+	const (
+		first  = iota
+		second = iota
+		third  = iota
+	)
+	//In each const block iota reset the number
+	const (
+		N1first  = iota
+		N1second = iota
+	)
+	fmt.Println(first, second, third)
+	fmt.Println(N1first, N1second)
+
 }
